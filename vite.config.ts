2c6d74ef4700cb7becb2548828a1ba673df32b28
Beslan <beslandudaev8@gmail.com>
2025-03-09 21:41:44 +0300

    as

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,11 +10,6 @@ export default defineConfig({
     outDir: "dist",  // Папка для сборки
     rollupOptions: {
       external: [],
-    },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
