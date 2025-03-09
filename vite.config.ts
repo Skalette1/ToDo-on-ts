68c0354bef3a967d26284b1563293616cedd408b
Beslan <beslandudaev8@gmail.com>
2025-03-09 21:10:55 +0300

    deploy on gh

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Skalette1/", // Укажите имя репозитория здесь
+  base: "/ToDo-on-ts/", // Укажите имя репозитория здесь
 });
